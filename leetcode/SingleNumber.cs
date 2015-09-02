f1176c0053fb84ca88e135cb7b7eb47a56d7f803
Snake Liu <yiyiyouxia@live.cn>
2015-09-02 15:40:26 +0800

    SingleNumber

---
+++ leetcode/SingleNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Test
+{
+    static void Main()
+    {
+        Console.WriteLine(SingleNumber(new []{1, 2, 3, 5, 9, 5, 1, 3, 2}));
+    }
+    public static int SingleNumber(int[] nums)
+    {
+        for (int i = nums.Length - 1; i > 0; i--)
+            nums[i - 1] ^= nums[i];
+
+        return nums[0];
+    }
+}
